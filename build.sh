b22f628e0ce070496212b477756fce0542f9e5cf
accosmin <accosmin@gmail.com>
2019-02-15 09:56:08 +0100

    fix setting up dependencies (Eigen3 & pthread)

--- build.sh
+++ build.sh
@@ -2,16 +2,18 @@
 
 basedir=$(pwd)
 
-#TODO: check exit codes
-
 # build and test library
 cd ${basedir}
-cmake -GNinja -Hlibnano -Bbuild/libnano -DCMAKE_BUILD_TYPE=Debug -DBUILD_SHARED_LIBS=OFF
-#cmake --build build/libnano --target install
-cd build/libnano && ninja -k1000 && ctest --output-on-failure && sudo ninja install
+cmake -GNinja -Hlibnano -Bbuild/libnano -DCMAKE_BUILD_TYPE=Debug -DBUILD_SHARED_LIBS=OFF || exit 1
+#cmake --build build/libnano --target install || exit 1
+cd build/libnano
+ninja -k1000 || exit 1
+ctest --output-on-failure || exit 1
+sudo ninja install || exit 1
 
 # build examples
 cd ${basedir}
-cmake -GNinja -Hexample -Bbuild/example -DCMAKE_BUILD_TYPE=Debug
-#cmake --build build/example
-cd build/example && ninja -k1000
+cmake -GNinja -Hexample -Bbuild/example -DCMAKE_BUILD_TYPE=Debug || exit 1
+#cmake --build build/example || exit 1
+cd build/example
+ninja -k1000 || exit 1
