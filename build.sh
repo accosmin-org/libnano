fb0cdad2d24c6df87e405c56f4f2c1d84d0822f3
accosmin <accosmin@gmail.com>
2019-02-17 22:43:22 +0100

    install test library in the local directory

--- build.sh
+++ build.sh
@@ -2,17 +2,20 @@
 
 basedir=$(pwd)
 
+installdir=${basedir}/install
+
 # build and test library
 cd ${basedir}
-cmake -GNinja -Hlibnano -Bbuild/libnano -DCMAKE_BUILD_TYPE=Debug -DBUILD_SHARED_LIBS=OFF || exit 1
+cmake -GNinja -Hlibnano -Bbuild/libnano -DCMAKE_BUILD_TYPE=Debug -DBUILD_SHARED_LIBS=ON -DCMAKE_INSTALL_PREFIX=${installdir} || exit 1
 #cmake --build build/libnano --target install || exit 1
 cd build/libnano
 ninja -k1000 || exit 1
 ctest --output-on-failure || exit 1
-sudo ninja install || exit 1
+ninja install || exit 1
 
 # build examples
 cd ${basedir}
+PATH=${PATH}:${installdir}
 cmake -GNinja -Hexample -Bbuild/example -DCMAKE_BUILD_TYPE=Debug || exit 1
 #cmake --build build/example || exit 1
 cd build/example
