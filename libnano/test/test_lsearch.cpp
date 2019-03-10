ed4449b12100c361db94e9df956f57fae3642b18
accosmin <accosmin@gmail.com>
2019-03-10 14:29:42 +0100

    fix unit test (line-search with backtracking strategy)

--- libnano/test/test_lsearch.cpp
+++ libnano/test/test_lsearch.cpp
@@ -1,3 +1,4 @@
+#include <iomanip>
 #include <utest/utest.h>
 #include <nano/solver.h>
 #include <nano/numeric.h>
@@ -19,12 +20,6 @@ UTEST_CASE(strategy_backtrack)
     lsearch->c1(scalar_t{1e-4});
     lsearch->c2(scalar_t{9e-1});
     lsearch->max_iterations(100);
-    lsearch->logger([] (const solver_state_t& state)
-    {
-        std::cout
-            << "\t: t=" << state.t << ",f=" << state.f
-            << ",g=" << state.convergence_criterion() << "\n";
-    });
 
     // check that the line-search doesn't fail from various starting points
     //  and the resulting point satisfies the Armijo condition
@@ -37,9 +32,19 @@ UTEST_CASE(strategy_backtrack)
             const auto t0 = scalar_t{1};
             const auto state0 = get_state0(function);
 
+            std::cout << std::setprecision(8) << "..x0=" << state0.x.transpose() << "\n";
+            lsearch->logger([&] (const solver_state_t& state)
+            {
+                std::cout << std::setprecision(8) << "    "
+                    << "t0=" << t0 << ",f0=" << state0.f
+                    << ",t=" << state.t << ",f=" << state.f
+                    << ",g=" << state.convergence_criterion()
+                    << ",armijo=" << state.has_armijo(state0, lsearch->c1()) << "\n";
+            });
+
             solver_state_t state = state0;
             UTEST_CHECK(lsearch->get(state0, t0, state));
-            UTEST_CHECK(state0.has_armijo(state0, lsearch->c1()));
+            UTEST_CHECK(state.has_armijo(state0, lsearch->c1()));
         }
     }
 }
