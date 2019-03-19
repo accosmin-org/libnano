1be2e618b56f397a20cb1a66f4ae1d4ced7996b6
accosmin <accosmin@gmail.com>
2019-03-19 14:32:50 +0100

    more robust LeMarechal line-search: decrease length if invalid state, safeguard cubic interpolation

--- libnano/src/lsearch/lemarechal.cpp
+++ libnano/src/lsearch/lemarechal.cpp
@@ -18,6 +18,16 @@ void lsearch_lemarechal_t::config(const json_t& json)
     nano::from_json_range(json, "ro", m_ro, 1 + eps, inf);
 }
 
+static scalar_t safeguard(const lsearch_step_t& a, const lsearch_step_t& b)
+{
+    const auto tmin = std::min(a.t, b.t);
+    const auto tmax = std::max(a.t, b.t);
+
+    const auto tc = lsearch_step_t::cubic(a, b);
+    const auto tb = lsearch_step_t::bisect(a, b);
+    return (std::isfinite(tc) && tc > tmin && tc < tmax) ? tc : tb;
+}
+
 bool lsearch_lemarechal_t::get(const solver_state_t& state0, scalar_t t, solver_state_t& state)
 {
     lsearch_step_t L = state0;
@@ -26,14 +36,15 @@ bool lsearch_lemarechal_t::get(const solver_state_t& state0, scalar_t t, solver_
     bool R_updated = false;
     for (int i = 1; i < max_iterations() && t < stpmax(); ++ i)
     {
-        if (!state.update(state0, t))
+        const auto ok = state.update(state0, t);
+        log(state);
+
+        if (!ok)
         {
-            log(state);
-            return false;
+            t *= 0.5;
         }
         else if (state.has_armijo(state0, c1()))
         {
-            log(state);
             if (state.has_wolfe(state0, c2()))
             {
                 return true;
@@ -48,16 +59,15 @@ bool lsearch_lemarechal_t::get(const solver_state_t& state0, scalar_t t, solver_
                 }
                 else
                 {
-                    t = lsearch_step_t::cubic(L, R);
+                    t = safeguard(L, R);
                 }
             }
         }
         else
         {
-            log(state);
             R = state;
             R_updated = true;
-            t = lsearch_step_t::cubic(L, R);
+            t = safeguard(L, R);
         }
     }
 
