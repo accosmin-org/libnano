74660dfdb299a86541b3a2929dea9a26910929ae
accosmin <accosmin@gmail.com>
2019-04-27 23:07:29 +0200

    toggable interpolation method for line-search using backtracking

--- libnano/src/lsearchk/backtrack.cpp
+++ libnano/src/lsearchk/backtrack.cpp
@@ -5,16 +5,18 @@ using namespace nano;
 json_t lsearchk_backtrack_t::config() const
 {
     json_t json;
+    json["interpolation"] = strcat(m_method, join(enum_values<lsearchk_backtrack_t::interpolation>()));
     return json;
 }
 
-void lsearchk_backtrack_t::config(const json_t&)
+void lsearchk_backtrack_t::config(const json_t& json)
 {
+    nano::from_json(json, "interpolation", m_method);
 }
 
 bool lsearchk_backtrack_t::get(const solver_state_t& state0, solver_state_t& state)
 {
-    for (int i = 0; i < max_iterations(); ++ i)
+    for (int i = 0; i < max_iterations() && state; ++ i)
     {
         if (state.has_armijo(state0, c1()))
         {
@@ -22,7 +24,24 @@ bool lsearchk_backtrack_t::get(const solver_state_t& state0, solver_state_t& sta
         }
 
         // next trial
-        state.update(state0, lsearch_step_t::interpolate(state0, state));
+        scalar_t t = 0;
+        switch (m_method)
+        {
+        case interpolation::cubic:
+            t = lsearch_step_t::cubic(state0, state);
+            if (std::isfinite(t) && t < state0.t && t > state.t)
+            {
+                break;
+            }
+            // NB: if cubic interpolation fails, fallback to bisection!
+
+        case interpolation::bisect:
+        default:
+            t = lsearch_step_t::bisect(state0, state);
+            break;
+        }
+
+        state.update(state0, t);
         log(state0, state);
     }
 
--- libnano/src/lsearchk/backtrack.h
+++ libnano/src/lsearchk/backtrack.h
@@ -12,10 +12,34 @@ namespace nano
     {
     public:
 
+        ///
+        /// \brief interpolation method using the information at step length zero and the current step length.
+        ///
+        enum class interpolation
+        {
+            bisect,                 ///<
+            cubic                   ///<
+        };
+
         lsearchk_backtrack_t() = default;
 
         json_t config() const final;
         void config(const json_t&) final;
         bool get(const solver_state_t& state0, solver_state_t& state) final;
+
+    private:
+
+        // attributes
+        interpolation   m_method{interpolation::cubic}; ///<
     };
+
+    template <>
+    inline enum_map_t<lsearchk_backtrack_t::interpolation> enum_string<lsearchk_backtrack_t::interpolation>()
+    {
+        return
+        {
+            { lsearchk_backtrack_t::interpolation::bisect,      "bisect"},
+            { lsearchk_backtrack_t::interpolation::cubic,       "cubic"}
+        };
+    }
 }
