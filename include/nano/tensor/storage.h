7bb9b8607511997719b9bf50932b194222dd3f1a
accosmin <accosmin@gmail.com>
2020-10-26 00:04:31 +0100

    fix some clang-tidy warnings

--- include/nano/tensor/storage.h
+++ include/nano/tensor/storage.h
@@ -182,7 +182,7 @@ namespace nano
             return *this;
         }
 
-        tensor_marray_storage_t& operator=(const tensor_marray_storage_t<tscalar>& other) // NOLINT(bugprone-unhandled-self-assignment)
+        tensor_marray_storage_t& operator=(const tensor_marray_storage_t<tscalar>& other) // NOLINT(cert-oop54-cpp,bugprone-unhandled-self-assignment)
         {
             if (this != &other)
             {
