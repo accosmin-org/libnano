aa7e973f60ec45e387746265b7dc1bf371ecc5a2
accosmin <accosmin@protonmail.com>
2022-05-09 22:48:08 +0200

    convergence criterion for pgm (wip)

--- src/solver/universal.cpp
+++ src/solver/universal.cpp
@@ -14,12 +14,16 @@ solver_pgm_t::solver_pgm_t()
 {
     monotonic(false);
 
-    register_parameter(parameter_t::make_float("solver::pgm::L0", 0.0, LT, 1.0, LT, 1e+12));
-    register_parameter(parameter_t::make_integer("solver::pgm::lsearch_max_iterations", 10, LE, 40, LE, 100));
+    static constexpr auto max = std::numeric_limits<scalar_t>::max();
+
+    register_parameter(parameter_t::make_float("solver::pgm::D", 0.0, LT, 1.0, LT, max));
+    register_parameter(parameter_t::make_float("solver::pgm::L0", 0.0, LT, 1.0, LT, max));
+    register_parameter(parameter_t::make_integer("solver::pgm::lsearch_max_iterations", 10, LE, 50, LE, 100));
 }
 
 solver_state_t solver_pgm_t::minimize(const function_t& function_, const vector_t& x0) const
 {
+    const auto D = parameter("solver::pgm::D").value<scalar_t>();
     const auto L0 = parameter("solver::pgm::L0").value<scalar_t>();
     const auto epsilon = parameter("solver::epsilon").value<scalar_t>();
     const auto max_evals = parameter("solver::max_evals").value<int64_t>();
@@ -27,47 +31,62 @@ solver_state_t solver_pgm_t::minimize(const function_t& function_, const vector_
 
     auto function = make_function(function_, x0);
     auto state = solver_state_t{function, x0};
-    auto bstate = state;    // best state so far
-    auto& xp = state.d;     // line-search step
+    auto bstate = state;        // best state so far
+    auto& xp = state.d;         // line-search step
+    const auto& f = state.f;
+    const auto& x = state.x;
+    const auto& g = state.g;
 
     scalar_t L = L0;
+    scalar_t Sk = 0.0;
+    vector_t Gk = vector_t::Zero(function.size());
 
-    for (int64_t i = 0; function.fcalls() < max_evals; ++ i, L *= 0.5)
+    for (int64_t i = 0; function.fcalls() < max_evals; ++ i)
     {
         // 1. line-search
-        auto M = L;
+        auto M = L, fp = f;
         auto iter_ok = false;
         auto converged = false;
         for (int64_t k = 0; k < lsearch_max_iterations && !iter_ok; ++ k, M *= 2.0)
         {
-            const auto& f = state.f;
-            const auto& x = state.x;
-            const auto& g = state.g;
             xp = bregman_mapping(x, g, M);
-
-            const auto fp = function.vgrad(xp);
+            fp = function.vgrad(xp);
             iter_ok = std::isfinite(fp) && fp <= f + g.dot(xp - x) + 0.5 * M * (xp - x).dot(xp - x) + 0.5 * epsilon;
         }
 
-        if (solver_t::done(function, bstate, iter_ok, converged))
+        if (iter_ok)
         {
-            break;
+            Sk += 2.0 / M;
+            Gk += state.g * 2.0 / M;
+
+            // 2. update current and best state (if the line-search step doesn't fail)
+            L = 0.5 * M;
+            state.update(xp);
+            if (state < bstate)
+            {
+                bstate.x = state.x;
+                bstate.f = state.f;
+                bstate.g = state.g;
+            }
+
+            // NB: There are two errors in the original paper regarding the convergence criterion:
+            // * the definition of Sk
+            // * the constraint eps(x0, y) <= D is applied with the wrong sign
+            const auto beta = std::sqrt(Gk.dot(Gk) / (2.0 * D)) / Sk;
+            converged = (Gk.dot(x0) / Sk + 2.0 * beta * D) < epsilon;
+
+            // TODO: check convergence!!!
+            //converged = (xp - x).lpNorm<Eigen::Infinity>() < epsilon && std::fabs(fp - f) < epsilon;
+
+
+            std::cout << std::fixed << std::setprecision(12) << "i=" << i
+                << ", f=" << state.f << "|" << bstate.f
+                << ", g=" << state.g.lpNorm<Eigen::Infinity>() << "|" << bstate.g.lpNorm<Eigen::Infinity>()
+                << ", L=" << L
+                << ", calls=" << function.fcalls() << "|" << function.gcalls()
+                << std::endl;
         }
 
-        // 2. update current and best state (if the line-search step doesn't fail)
-        state.update(xp);
-        if (state < bstate)
-        {
-            bstate = state;
-        }
-
-        std::cout << std::fixed << std::setprecision(12) << "i=" << i
-            << ", f=" << state.f << "|" << bstate.f
-            << ", g=" << state.g.lpNorm<Eigen::Infinity>() << "|" << bstate.g.lpNorm<Eigen::Infinity>()
-            << ", L=" << L
-            << std::endl;
-
-        // TODO: check convergence!!!
         iter_ok = static_cast<bool>(state);
         if (solver_t::done(function, bstate, iter_ok, converged))
         {
@@ -75,5 +94,5 @@ solver_state_t solver_pgm_t::minimize(const function_t& function_, const vector_
         }
     }
 
-    return state;
+    return bstate;
 }
