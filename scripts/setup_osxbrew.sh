73a30339304e153ee7c569f7efaa391fa8423a0a
accosmin <accosmin@protonmail.com>
2022-08-03 22:02:06 +0200

    fix osx deploy

--- scripts/setup_osxbrew.sh
+++ scripts/setup_osxbrew.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
-brew update
+#brew update
 #brew upgrade
-brew ls --versions cmake || brew install -f cmake
+#brew ls --versions cmake || brew install -f cmake
 brew ls --versions eigen || brew install -f eigen
+brew ls --versions ninja || brew install -f ninja
 brew ls --versions coreutils || brew install -f coreutils
