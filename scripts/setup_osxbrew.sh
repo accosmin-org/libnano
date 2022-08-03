47a7aabc863e058d76ce3788891e283b08232ad5
accosmin <accosmin@protonmail.com>
2022-08-03 22:00:09 +0200

    check deploying on osx 11

--- scripts/setup_osxbrew.sh
+++ scripts/setup_osxbrew.sh
@@ -4,3 +4,4 @@ brew update
 #brew upgrade
 brew ls --versions cmake || brew install -f cmake
 brew ls --versions eigen || brew install -f eigen
+brew ls --versions coreutils || brew install -f coreutils
