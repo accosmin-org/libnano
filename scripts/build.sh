173cb3947e88ac28128c4fa14ce855b760dcdc23
accosmin <accosmin@protonmail.com>
2023-03-14 21:50:26 +0100

    upgrade sonar scanner

--- scripts/build.sh
+++ scripts/build.sh
@@ -401,7 +401,7 @@ function clang_format {
 function sonar {
     cd ${basedir}
 
-    export SONAR_SCANNER_VERSION=4.7.0.2747
+    export SONAR_SCANNER_VERSION=4.8.0.2856
     export SONAR_SCANNER_HOME=$HOME/.sonar/sonar-scanner-$SONAR_SCANNER_VERSION-linux
     curl --create-dirs -sSLo $HOME/.sonar/sonar-scanner.zip \
         https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-$SONAR_SCANNER_VERSION-linux.zip
