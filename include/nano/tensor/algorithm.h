fa372ce5bc77ce496ce036cb3d23ca1a4379ed4b
accosmin <accosmin@protonmail.com>
2024-01-30 22:24:54 +0100

    simplify

--- include/nano/tensor/algorithm.h
+++ include/nano/tensor/algorithm.h
@@ -6,6 +6,14 @@ namespace nano
 {
 namespace detail
 {
+template <typename ttensor, typename... ttensors>
+auto size(const ttensor& tensor, const ttensors&... tensors)
+{
+    const auto size = tensor.template size<0>();
+    (assert(size == tensors.template size<0>()), ...);
+    return size;
+}
+
 template <typename ttensor>
 void copy(const tensor_size_t isrc, const tensor_size_t idst, ttensor& tensor)
 {
@@ -29,11 +37,11 @@ void copy(const tensor_size_t isrc, const tensor_size_t idst, ttensor& tensor)
 /// NB: the remaining ones are compacted starting the begining and their number is returned.
 /// NB: no allocation is performed.
 ///
-template <typename toperator, typename ttensor, typename... ttensors>
-auto remove_if(const toperator& op, ttensor& tensor, ttensors&... tensors) noexcept ->
-    typename std::enable_if_t<(is_tensor_v<ttensor> && ... && is_tensor_v<ttensors>), tensor_size_t>
+template <typename toperator, typename... ttensors>
+auto remove_if(const toperator& op, ttensors&... tensors) noexcept ->
+    typename std::enable_if_t<(is_tensor_v<ttensors> && ...), tensor_size_t>
 {
-    const auto size = tensor.template size<0>();
+    const auto size = detail::size(tensors...);
 
     auto last = tensor_size_t{0};
     for (; last < size && !op(last); ++last)
@@ -44,7 +52,6 @@ auto remove_if(const toperator& op, ttensor& tensor, ttensors&... tensors) noexc
     {
         if (!op(curr))
         {
-            detail::copy(curr, last, tensor);
             (detail::copy(curr, last, tensors), ...);
             ++last;
         }
