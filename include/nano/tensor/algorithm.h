7ce05982bb248257b4dfa4a8679fd73e837c89ec
Cosmin Atanasoaei <accosmin@users.noreply.github.com>
2024-01-31 09:25:33 +0100

    conservative remove_if for multiple tensors (#83)

---
+++ include/nano/tensor/algorithm.h
@@ -0,0 +1,61 @@
+#pragma once
+
+#include <nano/tensor/tensor.h>
+
+namespace nano
+{
+namespace detail
+{
+template <typename ttensor, typename... ttensors>
+auto size(const ttensor& tensor, const ttensors&...)
+{
+    const auto size = tensor.template size<0>();
+    return size;
+}
+
+template <typename ttensor>
+void copy(const tensor_size_t isrc, const tensor_size_t idst, ttensor& tensor)
+{
+    assert(isrc < tensor.template size<0>());
+    assert(idst < tensor.template size<0>());
+
+    if constexpr (ttensor::rank() == 1U)
+    {
+        tensor(idst) = tensor(isrc);
+    }
+    else
+    {
+        tensor.tensor(idst) = tensor.tensor(isrc);
+    }
+}
+} // namespace detail
+
+///
+/// \brief remove all sub-tensors indexed by the first dimension flagged by the given operator.
+///
+/// NB: the remaining ones are compacted starting the begining and their number is returned.
+/// NB: no allocation is performed.
+///
+template <typename toperator, typename... ttensors>
+auto remove_if(const toperator& op, ttensors&&... tensors) ->
+    typename std::enable_if_t<(is_tensor_v<std::remove_reference_t<ttensors>> && ...), tensor_size_t>
+{
+    const auto size = detail::size(std::forward<ttensors>(tensors)...);
+
+    auto last = tensor_size_t{0};
+    for (; last < size && !op(last); ++last)
+    {
+    }
+
+    for (auto curr = last; curr < size; ++curr)
+    {
+        if (!op(curr))
+        {
+            (detail::copy(curr, last, tensors), ...);
+            ++last;
+        }
+    }
+
+    return last;
+}
+} // namespace nano
---
+++ test/test_tensor_algorithm.cpp
@@ -0,0 +1,90 @@
+#include <nano/tensor/algorithm.h>
+#include <utest/utest.h>
+
+using namespace nano;
+
+namespace
+{
+template <typename toperator, typename... ttensors>
+auto copy_remove_if(const toperator& op, ttensors... tensors)
+{
+    const auto count = nano::remove_if(op, tensors...);
+    return std::make_tuple(count, tensors...);
+}
+} // namespace
+
+UTEST_BEGIN_MODULE(test_tensor_algorithm)
+
+UTEST_CASE(vector)
+{
+    const auto xdata = make_vector<int>(0, 1, 2, 3, 4, 5, 6, 7);
+    {
+        const auto op = [&](const tensor_size_t) { return true; };
+
+        const auto [size, data] = copy_remove_if(op, xdata);
+        UTEST_CHECK_EQUAL(size, 0);
+        UTEST_CHECK_EQUAL(data, make_vector<int>(0, 1, 2, 3, 4, 5, 6, 7));
+    }
+    {
+        const auto op = [&](const tensor_size_t i) { return xdata(i) % 3 == 2; };
+
+        const auto [size, data] = copy_remove_if(op, xdata);
+        UTEST_CHECK_EQUAL(size, 6);
+        UTEST_CHECK_EQUAL(data, make_vector<int>(0, 1, 3, 4, 6, 7, 6, 7));
+    }
+    {
+        const auto op = [&](const tensor_size_t i) { return xdata(i) > 10; };
+
+        const auto [size, data] = copy_remove_if(op, xdata);
+        UTEST_CHECK_EQUAL(size, 8);
+        UTEST_CHECK_EQUAL(data, make_vector<int>(0, 1, 2, 3, 4, 5, 6, 7));
+    }
+}
+
+UTEST_CASE(mixed)
+{
+    const auto xvec = make_vector<int>(0, 1, 2, 3, 4, 5, 6, 7);
+    const auto xmat = make_matrix<int>(8, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15);
+    {
+        const auto op = [&](const tensor_size_t) { return true; };
+
+        const auto [size, vec, mat] = copy_remove_if(op, xvec, xmat);
+        UTEST_CHECK_EQUAL(size, 0);
+        UTEST_CHECK_EQUAL(vec, make_vector<int>(0, 1, 2, 3, 4, 5, 6, 7));
+        UTEST_CHECK_EQUAL(mat, make_matrix<int>(8, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15));
+    }
+    {
+        const auto op = [&](const tensor_size_t i) { return i == 0; };
+
+        const auto [size, vec, mat] = copy_remove_if(op, xvec, xmat);
+        UTEST_CHECK_EQUAL(size, 7);
+        UTEST_CHECK_EQUAL(vec, make_vector<int>(1, 2, 3, 4, 5, 6, 7, 7));
+        UTEST_CHECK_EQUAL(mat, make_matrix<int>(8, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 14, 15));
+    }
+    {
+        const auto op = [&](const tensor_size_t i) { return i > 5; };
+
+        const auto [size, vec, mat] = copy_remove_if(op, xvec, xmat);
+        UTEST_CHECK_EQUAL(size, 6);
+        UTEST_CHECK_EQUAL(vec, make_vector<int>(0, 1, 2, 3, 4, 5, 6, 7));
+        UTEST_CHECK_EQUAL(mat, make_matrix<int>(8, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15));
+    }
+    {
+        const auto op = [&](const tensor_size_t i) { return xmat(i, 0) + xmat(i, 1) == 9; };
+
+        const auto [size, vec, mat] = copy_remove_if(op, xvec, xmat);
+        UTEST_CHECK_EQUAL(size, 7);
+        UTEST_CHECK_EQUAL(vec, make_vector<int>(0, 1, 3, 4, 5, 6, 7, 7));
+        UTEST_CHECK_EQUAL(mat, make_matrix<int>(8, 0, 1, 2, 3, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 14, 15));
+    }
+    {
+        const auto op = [&](const tensor_size_t i) { return (xmat(i, 0) + xmat(i, 1) == 9) || (xvec(i) > 4); };
+
+        const auto [size, vec, mat] = copy_remove_if(op, xvec, xmat);
+        UTEST_CHECK_EQUAL(size, 4);
+        UTEST_CHECK_EQUAL(vec, make_vector<int>(0, 1, 3, 4, 4, 5, 6, 7));
+        UTEST_CHECK_EQUAL(mat, make_matrix<int>(8, 0, 1, 2, 3, 6, 7, 8, 9, 8, 9, 10, 11, 12, 13, 14, 15));
+    }
+}
+
+UTEST_END_MODULE()
