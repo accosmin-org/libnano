930d4798f25af114dc33e9deb1f8ac1ea9a49e1d
accosmin <accosmin@protonmail.com>
2025-02-23 13:21:31 +0100

    numerically more robust gradient descent algorithm - switch back to previous state when the current one diverges

--- src/solver/gd.cpp
+++ src/solver/gd.cpp
@@ -25,27 +25,29 @@ solver_state_t solver_gd_t::do_minimize(const function_t& function, const vector
 
     const auto max_evals = parameter("solver::max_evals").value<tensor_size_t>();
 
-    auto state = solver_state_t{function, x0};
-    if (state.gx().lpNorm<Eigen::Infinity>() < epsilon0<scalar_t>())
+    auto cstate = solver_state_t{function, x0};
+    if (cstate.gx().lpNorm<Eigen::Infinity>() < epsilon0<scalar_t>())
     {
-        solver_t::done_gradient_test(state, state.valid(), logger);
-        return state;
+        solver_t::done_gradient_test(cstate, cstate.valid(), logger);
+        return cstate;
     }
 
+    auto pstate  = cstate;
     auto lsearch = make_lsearch();
     auto descent = vector_t{function.size()};
 
     while (function.fcalls() + function.gcalls() < max_evals)
     {
-        descent = -state.gx();
+        descent = -cstate.gx();
+        pstate  = cstate;
 
         // line-search
-        const auto iter_ok = lsearch.get(state, descent, logger);
-        if (solver_t::done_gradient_test(state, iter_ok, logger))
+        const auto iter_ok = lsearch.get(cstate, descent, logger);
+        if (solver_t::done_gradient_test(cstate, iter_ok, logger))
         {
             break;
         }
     }
 
-    return state;
+    return cstate.valid() ? cstate : pstate;
 } // LCOV_EXCL_LINE
