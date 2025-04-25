1ce8a039d58651c2adfe6383a116512bd529e4c4
accosmin <accosmin@protonmail.com>
2025-04-26 00:28:45 +0200

    fix scaling for G and A

--- src/solver/interior/util.cpp
+++ src/solver/interior/util.cpp
@@ -25,9 +25,12 @@ auto delta(const Qrow& qrow, const Grow& grow, const Arow& arow)
     return std::max({Qdelta, Gdelta, Adelta});
 }
 
-auto scale(const scalar_t row_norm, const scalar_t tau)
+void scale(const scalar_t row_norm, const scalar_t tau, scalar_t& current_scale)
 {
-    return (row_norm > tau) ? (1.0 / std::sqrt(row_norm)) : 1.0;
+    if (row_norm > tau)
+    {
+        current_scale = 1.0 / std::sqrt(row_norm);
+    }
 }
 } // namespace
 
@@ -70,6 +73,7 @@ void nano::modified_ruiz_equilibration(vector_t& dQ, matrix_t& Q, vector_t& c, v
     auto cA = make_full_vector<scalar_t>(p, 1.0);
 
     // TODO: the scaling factor `c` from (2) is not working!
+    // TODO: the scaling factor `gamma` from (2) is not working!
 
     for (auto k = 0; k < max_iters && (k == 0 || ::delta(cQ, cG, cA) > tolerance); ++k)
     {
@@ -77,15 +81,15 @@ void nano::modified_ruiz_equilibration(vector_t& dQ, matrix_t& Q, vector_t& c, v
 
         for (tensor_size_t i = 0; i < n; ++i)
         {
-            cQ(i) = ::scale(::inorm(Q.row(i), G.col(i), A.col(i)), tau);
+            ::scale(::inorm(Q.row(i), G.col(i), A.col(i)), tau, cQ(i));
         }
         for (tensor_size_t i = 0; i < m; ++i)
         {
-            cG(i) = ::scale(G.row(i).lpNorm<Eigen::Infinity>(), tau);
+            ::scale(G.row(i).lpNorm<Eigen::Infinity>(), tau, cG(i));
         }
         for (tensor_size_t i = 0; i < p; ++i)
         {
-            cA(i) = ::scale(A.row(i).lpNorm<Eigen::Infinity>(), tau);
+            ::scale(A.row(i).lpNorm<Eigen::Infinity>(), tau, cA(i));
         }
 
         Q.matrix().noalias() = cQ.vector().asDiagonal() * Q * cQ.vector().asDiagonal();
@@ -96,13 +100,13 @@ void nano::modified_ruiz_equilibration(vector_t& dQ, matrix_t& Q, vector_t& c, v
         h.array() *= cG.array();
         b.array() *= cA.array();
 
-        const auto gamma =
+        /*const auto gamma =
             1.0 / std::max(Q.matrix().rowwise().lpNorm<Eigen::Infinity>().mean(), c.lpNorm<Eigen::Infinity>());
 
         std::cout << "k=" << k << ",gamma=" << gamma << std::endl;
 
         Q.array() *= gamma;
-        c.array() *= gamma;
+        c.array() *= gamma;*/
 
         dQ.array() *= cQ.array();
         dG.array() *= cG.array();
