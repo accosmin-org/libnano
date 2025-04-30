e7955f16892d3c3f495a0678067f817d86c8f2de
accosmin <accosmin@protonmail.com>
2025-04-30 23:03:36 +0200

    add support for ubuntu 24.04 in docker dev builds

--- docker/build.sh
+++ docker/build.sh
@@ -1,9 +1,16 @@
 #!/usr/bin/env bash
 
 ALPINE_VERSION=3.21
+UBUNTU_VERSION=24.04
 
 podman build \
     -f docker/Alpine.Dockerfile \
     --build-arg ALPINE_VERSION=${ALPINE_VERSION} \
     -t libnano-dev:alpine-${ALPINE_VERSION} \
+    .
+
+podman build \
+    -f docker/Ubuntu.Dockerfile \
+    --build-arg UBUNTU_VERSION=${UBUNTU_VERSION} \
+    -t libnano-dev:ubuntu-${UBUNTU_VERSION} \
     .
--- docker/run.sh
+++ docker/run.sh
@@ -1,9 +1,16 @@
 #!/usr/bin/env bash
 
 ALPINE_VERSION=3.21
+UBUNTU_VERSION=24.04
+
+# podman run \
+#    --userns=keep-id --security-opt label=disable \
+#    -v $(pwd):/code -w /code \
+#    libnano-dev:alpine-${ALPINE_VERSION} \
+#    "$@"
 
 podman run \
     --userns=keep-id --security-opt label=disable \
     -v $(pwd):/code -w /code \
-    libnano-dev:alpine-${ALPINE_VERSION} \
+    libnano-dev:ubuntu-${UBUNTU_VERSION} \
     "$@"
