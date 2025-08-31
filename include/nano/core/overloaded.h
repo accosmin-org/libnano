d459bfaa4b62b7934b73ae1c3dcdea8a4c0e55c5
accosmin <accosmin@protonmail.com>
2025-08-31 22:14:08 +0200

    fix build examples

--- include/nano/core/overloaded.h
+++ include/nano/core/overloaded.h
@@ -5,13 +5,9 @@
 /// supported type.
 ///
 /// NB: see https://en.cppreference.com/w/cpp/utility/variant/visit.
-/// FIXME: this won't be needed in C++20
 ///
 template <class... Ts>
 struct overloaded : Ts...
 {
     using Ts::operator()...;
 };
-
-template <class... Ts>
-overloaded(Ts...) -> overloaded<Ts...>;
