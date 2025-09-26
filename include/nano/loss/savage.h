00dc7fbcbaaea42e75a5acda3cc08fc8fa39c553
accosmin <accosmin@protonmail.com>
2025-09-26 23:34:58 +0200

    fix savage loss

--- include/nano/loss/savage.h
+++ include/nano/loss/savage.h
@@ -18,28 +18,28 @@ struct savage_t : public terror
     requires is_eigen_v<tarray>
     static auto value(const tarray& target, const tarray& output)
     {
-        const auto edges = (target * output).exp();
+        const auto epos = (target * output).exp();
 
-        return (1.0 / (1.0 + edges)).square().sum();
+        return (1.0 / (1.0 + epos).square()).sum();
     }
 
     template <class tarray, class tgarray>
     requires(is_eigen_v<tarray> && is_eigen_v<tgarray>)
     static void vgrad(const tarray& target, const tarray& output, tgarray vgrad)
     {
-        const auto edges = (target * output).exp();
+        const auto epos = (target * output).exp();
+        const auto eneg = (-target * output).exp();
 
-        vgrad = -2.0 * target * (1.0 / (1.0 + edges)).square() * (edges / (1.0 + edges));
+        vgrad = -2.0 * target / ((1.0 + epos).square() * (1.0 + eneg));
     }
 
     template <class tarray, class thmatrix>
     requires(is_eigen_v<tarray> && is_eigen_v<thmatrix>)
     static void vhess(const tarray& target, const tarray& output, thmatrix vhess)
     {
-        const auto edges = (target * output).exp();
+        const auto epos = (target * output).exp();
 
-        vhess = (-2.0 * target * target * ((edges - 2.0 * edges) / edges) * (edges / (1.0 + edges)) *
-                 (1.0 / (1.0 + edges)).square())
+        vhess = (-2.0 * target * target * epos * (1.0 - 2.0 * epos) / ((1.0 + epos).square() * (1.0 + epos).square()))
                     .matrix()
                     .asDiagonal();
     }
